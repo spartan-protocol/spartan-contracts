d6ceb8d7a123eb57d925160c7e216849bc554116
verifyfirst <verify.first@protonmail.com>
2022-05-11 20:25:20 +0930

    IBEP20
    Included IBEP20

--- contracts/Swim.sol
+++ contracts/Swim.sol
@@ -1,42 +1,127 @@
-pragma solidity ^0.8.0;
-import "./Utilities.sol"; 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.3;
+import "./iBEP20.sol"; 
 
-contract Swim {
-    uint256 initOnce;
 
-mapping(address => PoolData) public mapToken_poolData;
+contract Swim is iBEP20 {  
+    address public immutable ASSET;  //Settlement Asset
+    address public immutable TOKEN;  //Paired Token
+    uint256 public AssetDepth;       //Settlement Asset Depth
+    uint256 public TokenDepth;       //Pair Token Depth
+
+    string private _name;                                                  //Name of Aquarium 
+    string private _symbol;                                                //Aquarium Symbol
+    uint8 public override immutable decimals;                              //Decimals of Aquarium A
+    uint256 public override totalSupply;
+    mapping(address => uint) private _balances;
+    mapping(address => mapping(address => uint)) private _allowances;
     
-address[] public pools;
-struct PoolData {
-        bool listed;
-        uint balanceBase;
-        uint balanceToken;
-        address[] spartans;
-        uint256 totalUnits;
-        mapping(address => uint) spartanUnits;
-}
+   
+    constructor () {
+      
+    }
+
+    //========================================iBEP20=========================================//
+
+    function name() external view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() external view override returns (string memory) {
+        return _symbol;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function transfer(address recipient, uint256 amount) external virtual override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external virtual override returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) external virtual returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) external virtual returns (bool) {
+        uint256 currentAllowance = _allowances[msg.sender][spender];
+        require(currentAllowance >= subtractedValue, "!approval");
+        _approve(msg.sender, spender, currentAllowance - subtractedValue);
+        return true;
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "!owner");
+        require(spender != address(0), "!spender");
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+        uint256 currentAllowance = _allowances[sender][msg.sender];
+        require(currentAllowance >= amount, "!approval");
+        _approve(sender, msg.sender, currentAllowance - amount);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "!sender");
+        require(recipient != address(0), '!BURN');
+        uint256 senderBalance = _balances[sender];
+        require(senderBalance >= amount, "!balance");
+        _balances[sender] = senderBalance - amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "!account");
+        totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function burn(uint256 amount) external virtual override {
+        _burn(msg.sender, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "!account");
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "!balance");
+        _balances[account] = accountBalance - amount;
+        totalSupply -= amount;
+        emit Transfer(account, address(0), amount);
+    }
+
+    //====================================POOL FUNCTIONS =================================//
 
 
-function initialize() private {
-    require(initOnce !=  1);
-    //do something
-    initOnce = 1;
-}
+    function add() external returns (){
+   
+    }
 
-function add() private {
-
-}
-
-function remove() private {
+    function remove() external returns () {
     
-}
+    }
 
-function swap() private {
+    function swap() external  returns () {
+        
+    }
+
     
-}
-
-function listToken() private {
-
-}
+ 
 
 }
